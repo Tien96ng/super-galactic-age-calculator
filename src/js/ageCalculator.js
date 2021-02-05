c93895ddc7fd0443995890d7ffd10d57be11a34b
tien96ng <tien96ng@ghmail.com>
2021-02-05 11:58:02 -0800

    Refactor venusAge logic and pass testing.

--- src/js/ageCalculator.js
+++ src/js/ageCalculator.js
@@ -2,6 +2,6 @@ export default class AgeCalculator {
   constructor(earthAge) {
     this.earthAge = earthAge;
     this.mercuryAge = parseFloat((earthAge / .24).toFixed(2));
-    this.venusAge;
+    this.venusAge= parseFloat((earthAge / .62).toFixed(2));
   }
 }
