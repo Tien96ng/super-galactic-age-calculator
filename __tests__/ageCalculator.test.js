9b5700233a3a5a3be060ae5520503085f70cb492
tien96ng <tien96ng@ghmail.com>
2021-02-05 11:52:31 -0800

    Pass testing for the person's mercuryAge.

--- __tests__/ageCalculator.test.js
+++ __tests__/ageCalculator.test.js
@@ -7,7 +7,11 @@ describe("AgeCalculator", () => {
     person = new AgeCalculator(15);
   });
 
-  test("Should return the age given earth age.", () => {
+  test("Should return the given earth age.", () => {
     expect(person.earthAge).toEqual(15);
+  });
+
+  test("Should return the given earth age in Mercury years.", () => {
+    expect(person.mercuryAge).toEqual(62.5);
   });
 });
